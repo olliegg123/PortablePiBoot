ef40fbc83a98fd096c1992b0d1435c3e799bf8f9
ogerrard <ogerrard@cisco.com>
2024-08-18 18:22:38 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# wget -O - https://github.com/olliegg123/PortablePiBoot/blob/main/install.sh | bash
 echo "Running setup script"
+read -p "Tailscale Auth Key:" ts_key
+read -p "Meraki API Key:" meraki_key
+read -p "Webex Bot Token:" webex_key
+read -p "ThousandEyes Token:" te_key
 echo "Updating packages"
 sudo apt-get update && sudo apt-get -y upgrade
+echo "Connecting to TailScale network"
+curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up --authkey $ts_key
+echo "Cloning necessary repo..."
+git clone https://github.com/olliegg123/PortablePiBoot
 echo "Installing MQTT Broker..."
 sudo apt install -y mosquitto mosquitto-clients
 sudo systemctl enable mosquitto.service
