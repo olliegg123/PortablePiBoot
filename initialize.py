d86068e3e5573a45b2840be74a234ac51729d7f9
ogerrard <ogerrard@cisco.com>
2024-08-19 15:46:14 -0700

    Update initialize.py

--- initialize.py
+++ initialize.py
@@ -10,6 +10,7 @@ import json
 from datetime import datetime, timedelta
 import sqlite3
 import random
+import netifaces as ni
 
 with open('/home/pi/keys.json') as f:
     keys = json.load(f)
@@ -48,8 +49,84 @@ for network in networkList:
 
 res = requests.get(url=base_url+f"organizations/{portable_orgId}/devices?networkIds[]={networkId}", headers=headers)
 myDeviceList = json.loads(res.text)
+print("Look at all my devices:")
 for device in myDeviceList:
-    print("Look at all my devices:")
     model = device["model"]
     serial = device["serial"]
     print(f"{model} - {serial}")
+#-----------------------------------------------------------------#
+print("Renaming Devices to be their model")
+for device in myDeviceList:
+    serial = device["serial"]
+    model = device["model"]
+    body = { 
+        "name": f"{model}",
+    }
+    print(body)
+    response = requests.put(url=base_url+f"devices/{serial}", headers=headers, json=body)
+    print(res.status_code)
+print("Changing MR Firewall to allow local on all SSIDs")
+for x in range(10):
+    body = { 
+            "allowLanAccess": True
+        }
+    res = requests.put(url=base_url+f"networks/{networkId}/wireless/ssids/{x}/firewall/l3FirewallRules", headers=headers, json=body)
+    print(res.status_code)
+
+
+print("Configuring MT/MS Relationships")
+body = {
+  "livestream": {
+    "relatedDevices": [
+    ]
+  }
+}
+for device in myDeviceList:
+    if device["model"][:2] == "MT":
+        body["livestream"]["relatedDevices"].append({"serial": device["serial"]})
+print(body)
+for device in myDeviceList:
+    if device["model"][:2] == "MV":
+        serial = device["serial"]
+        res = requests.put(url=base_url+f"devices/{serial}/sensor/relationships/", headers=headers, json=body)
+        print(res.status_code)
+        print(res.text)
+
+print("Creating Meraki IOT MQTT Broker")
+raspberryIp = str(ni.ifaddresses('eth0')[ni.AF_INET][0]['addr'])
+body = {
+  "name": "Pi Broker",
+  "host": raspberryIp,
+  "port": 1883
+  }
+res = requests.post(url=base_url+f"networks/{networkId}/mqttBrokers", headers=headers, json=body)
+print(res.status_code)
+r = json.loads(res.text)
+print(r)
+brokerId = r["id"]
+print(brokerId)
+
+
+print("Enabling MV Sense on Cameras and assigning MQTT Broker to MV")
+for device in myDeviceList:
+    if device["model"][:2] == "MV":
+        body = {
+            "senseEnabled": True,
+            "mqttBrokerId": brokerId,
+            "audioDetection": {
+                "enabled": False
+            }
+        }
+        res = requests.put(url=base_url+f"devices/{serial}/camera/sense/", headers=headers, json=body)
+        print(res.status_code)
+        print(res.text)
+
+print("Assigning broker to MTs")
+body = {
+  "enabled": True
+}
+testUrl = base_url+f"devices/{networkId}/sensor/mqttBrokers/{brokerId}"
+print(testUrl)
+res = requests.put(url=base_url+f"networks/{networkId}/sensor/mqttBrokers/{brokerId}", headers=headers, json=body)
+print(res.status_code)
+print(res.text)
